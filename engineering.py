c814f89d53f3a1cb13cd0b1b3a5cab56d74514e3
Γεώργιος Σεμερτζάκης <gsemertzakis@hotmail.com>
2021-10-15 14:49:58 +0300

    engineering

--- engineering.py
+++ engineering.py
@@ -1,9 +1,12 @@
 import re
 import string
-import pandas as pd
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
+
+
+english_stopwords = stopwords.words("english")
+wnl = WordNetLemmatizer()
 
 
 class TextParsing:
@@ -21,23 +24,4 @@ class TextParsing:
         tokens = word_tokenize(self.text)
         token_words = " ".join([wnl.lemmatize(word) for word in tokens if word not in english_stopwords])
         return token_words
-
-
-corona = pd.read_csv("C:/Coding Repository/covid-19-tweets-nlp/Corona_NLP_train.csv", encoding='ISO-8859-1')
-
-english_stopwords = stopwords.words("english")
-wnl = WordNetLemmatizer()
-
-corona["OriginalTweet"] = corona["OriginalTweet"].apply(lambda x: TextParsing(x).regexp())
-corona["OriginalTweet"] = corona["OriginalTweet"].apply(lambda x: TextParsing(x).tokenization())
-
-corona["Sentiment_integer"] = corona["Sentiment"].map({"Extremely Negative": 0,
-                                                       "Negative": 1,
-                                                       "Neutral": 2,
-                                                       "Positive": 3,
-                                                       "Extremely Positive": 4})
-
-corona.drop(["Location", "UserName", "ScreenName", "TweetAt"], axis=1, inplace=True)
-
-corona.to_csv("app_data/Corona_NLP_train_cleaned.csv", header=True, index=False)
 
